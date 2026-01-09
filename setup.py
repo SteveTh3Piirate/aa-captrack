1ec48cec40d6735df1199234ada6cbee22658010
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-09 22:18:42 +0000

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-captrack",
-    version="0.1.1",
+    version="0.1.2",
     description="Capital ship movement early warning plugin for AllianceAuth",
     packages=find_packages(),
     include_package_data=True,
