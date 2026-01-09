ee04f4b151ea7d27a2e541fc9930a6142178f9b2
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-09 20:58:08 +0000

    Bump version to 0.1.1 to refresh wheel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-captrack",
-    version="0.1.0",
+    version="0.1.1",
     description="Capital ship movement early warning plugin for AllianceAuth",
     packages=find_packages(),
     include_package_data=True,
