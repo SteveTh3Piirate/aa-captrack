a8c0dc9cb0b8e7a2eaa0e4b0cf6c02ef32e6d4fd
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-10 19:40:40 +0000

    admin panel webhook integration

--- captrack/admin.py
+++ captrack/admin.py
@@ -6,7 +6,7 @@ from eveuniverse.models import EveRegion
 @admin.register(CapTrackSettings)
 class CapTrackSettingsAdmin(admin.ModelAdmin):
     autocomplete_fields = ("blacklisted_regions",)
-    fields = ("blacklisted_regions",)
+    fields = ("blacklisted_regions", "webhook_url")
 
     def has_add_permission(self, request):
         # Only allow one settings row
@@ -16,8 +16,8 @@ class CapTrackSettingsAdmin(admin.ModelAdmin):
         if db_field.name == "blacklisted_regions":
             kwargs["queryset"] = (
                 EveRegion.objects.all()
-                .exclude(name__regex=r"^[A-Z]-R\d{5}$")      # wormhole pseudo-regions
-                .exclude(name__regex=r"^[A-Z]{1,2}-\d{2}$")  # wormhole constellations like VR-01
+                .exclude(name__regex=r"^[A-Z]-R\d{5}$")
+                .exclude(name__regex=r"^[A-Z]{1,2}-\d{2}$")
             )
 
         return super().formfield_for_manytomany(db_field, request, **kwargs)
