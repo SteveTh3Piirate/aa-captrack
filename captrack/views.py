cf615e92652c00ee28ce3bfee346f0abbbe16c1f
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-10 17:09:31 +0000

    views and dash update

--- captrack/views.py
+++ captrack/views.py
@@ -1,4 +1,16 @@
 ﻿from django.shortcuts import render
+from .models import BlacklistedRegion
+from .services import get_capitals_in_blacklisted_regions
 
 def dashboard(request):
-    return render(request, "captrack/dashboard.html", {})
+    blacklisted_regions = BlacklistedRegion.objects.all()
+    groups = get_capitals_in_blacklisted_regions()
+
+    return render(
+        request,
+        "captrack/dashboard.html",
+        {
+            "blacklisted_regions": blacklisted_regions,
+            "groups": groups,
+        }
+    )
