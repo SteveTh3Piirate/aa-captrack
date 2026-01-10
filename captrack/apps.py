4c7597957dc8cff6d60c908cfd8904810bc26519
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-10 00:19:08 +0000

    apps.py update

--- captrack/apps.py
+++ captrack/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from allianceauth.services.hooks import UrlHook
+from django.urls import include, path
 
 class CapTrackConfig(AppConfig):
     name = "captrack"
     label = "captrack"
     verbose_name = "CapTrack"
+
+    def ready(self):
+        UrlHook(
+            urlconf_module="captrack.urls",
+            namespace="captrack",
+            url_pattern="captrack/"
+        )
