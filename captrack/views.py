3f75bd595371ca36195cac4563bfcbaa04bdc941
SteveTh3Piirate <steveswindlehurst1991@gmail.com>
2026-01-10 17:16:39 +0000

    views model change

--- captrack/views.py
+++ captrack/views.py
@@ -1,9 +1,11 @@
 ﻿from django.shortcuts import render
-from .models import BlacklistedRegion
+from .models import CapTrackSettings
 from .services import get_capitals_in_blacklisted_regions
 
 def dashboard(request):
-    blacklisted_regions = BlacklistedRegion.objects.all()
+    settings = CapTrackSettings.objects.first()
+    blacklisted_regions = settings.blacklisted_regions.all() if settings else []
+
     groups = get_capitals_in_blacklisted_regions()
 
     return render(
